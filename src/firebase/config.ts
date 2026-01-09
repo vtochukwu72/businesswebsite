1dabcfc55078fa501378c0f991378481233b0025
victor tochukwu <vtochukwu72@gmail.com>
2026-01-09 12:14:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7642317981-d7dbe",
+  "appId": "1:450066594534:web:bd7704667c90666e529927",
+  "apiKey": "AIzaSyDTgGO2nUVrD3ThI-a2KYWhSVrDBiZF-6c",
+  "authDomain": "studio-7642317981-d7dbe.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "450066594534"
+};
