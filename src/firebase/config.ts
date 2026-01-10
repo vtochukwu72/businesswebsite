ad40a5068e96ca535b10a2e3e77badea7850e4e0
victor tochukwu <vtochukwu72@gmail.com>
2026-01-10 17:49:47 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,11 @@
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-    apiKey: "AIzaSyDTgGO2nUVrD3ThI-a2KYWhSVrDBiZF-6c",
-    authDomain: "studio-7642317981-d7dbe.firebaseapp.com",
-    projectId: "studio-7642317981-d7dbe",
-    storageBucket: "studio-7642317981-d7dbe.appspot.com",
-    messagingSenderId: "450066594534",
-    appId: "1:450066594534:web:bd7704667c90666e529927"
-  };
-  
+  apiKey: "AIzaSyAx76XZFjtjDP7j-xLizSs9sTV_kmF3Imk",
+  authDomain: "e-commerce-936b5.firebaseapp.com",
+  projectId: "e-commerce-936b5",
+  storageBucket: "e-commerce-936b5.appspot.com",
+  messagingSenderId: "84391388232",
+  appId: "1:84391388232:web:cbf2ca7870f51bd522e47d",
+  measurementId: "G-LLSFPMWG37"
+};
