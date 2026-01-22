8f5b043c7a4de9addac9f5e969889566d47c696e
victor tochukwu <vtochukwu72@gmail.com>
2026-01-22 11:24:22 +0000

    ## Error Type Runtime Error
    ## Error Message
    Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.aromaespejo.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
